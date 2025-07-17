e0ea7f062fd5080745e69049bfb24f01bb30b856
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-17 17:28:44 +0300

    a

--- run-on-aws.sh
+++ run-on-aws.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-aws ec2 run-instances --region eu-central-1 --image-id ami-0af9b40b1a16fe700 --instance-type t4g.nano --user-data file://user-data.sh
+
+# AMD64
+# aws ec2 run-instances --region eu-central-1 --image-id ami-0af9b40b1a16fe700 --instance-type t3.nano --user-data file://user-data.sh
+
+# ARM64
+aws ec2 run-instances --region eu-central-1 --image-id ami-0bf5eaabef51e7d39 --instance-type t4g.nano --user-data file://user-data.sh
+
